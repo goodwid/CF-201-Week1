675adebf505cea718d1d5ea601302cbc7dd11994
goodwid <goodwid@gmail.com>
2016-03-15 16:38:16 -0700

    Created working function for questions

--- page/js/lab.js
+++ page/js/lab.js
@@ -1,18 +1,36 @@
 // 201
 // 3-tue-lecture code demo
 
+var correctResponses = 0;
+function askQuestion (question, correctAnswer, correctFollowup, incorrectFollowup, ) { // Expecting Y or N passed as correctAnswer
+  console.log ('Asking '+question+', Correct answer is '+correctAnswer);
+  var answer = prompt(question);
+  console.log ('Response: '+answer);
+  modAnswer = answer[0].toUpperCase();
+  console.log (modAnswer);
+
+  if (modAnswer === correctAnswer) {
+    return true;
+  } else  {
+    return false;
+  }
+}
+
 var userName = prompt('What is your name?');
 console.log('The user said their name is ' + userName);
 
-alert('Hey there, ' + userName + ' I\'m going to ask you some questions about me. Please click "ok", then answer Y or N to the next question.');
+alert('Hi, ' + userName + '!  Prepare for some questions about me. Please click "ok", then answer Y or N to the next question.');
 
-var userResponse = prompt('Was, I born on the moon?');
-console.log('userResponse: ' + userResponse);
+console.log(askQuestion ('Was I born on the moon','Y', ""));
 
-if (userResponse.toLowerCase() === 'n' || userResponse.toUpperCase() === 'NO') {
-  alert('Nope, I was actualy born on the moon. I\'m a moonian computer language.');
-} else if (userResponse === 'Y') { // TODO: Make this more robust!
-  alert('Nice, I was born on the moon');
-} else {
-  alert('Ooops!, you should have given a Y or N, so you lose this round.');
-}
+
+// var userResponse = prompt('Was, I born on the moon?');
+// console.log('userResponse: ' + userResponse);
+//
+// if (userResponse.toLowerCase() === 'n' || userResponse.toUpperCase() === 'NO') {
+//   alert('Nope, I was actualy born on the moon. I\'m a moonian computer language.');
+// } else if (userResponse === 'Y') { // TODO: Make this more robust!
+//   alert('Nice, I was born on the moon');
+// } else {
+//   alert('Ooops!, you should have given a Y or N, so you lose this round.');
+// }
