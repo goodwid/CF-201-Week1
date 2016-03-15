b3d61d2ffb15eaa887b4b76f18a77c75922cd780
goodwid <goodwid@gmail.com>
2016-03-15 16:55:38 -0700

    Created array of question objects

--- page/js/lab.js
+++ page/js/lab.js
@@ -1,28 +1,55 @@
 // 201
 // 3-tue-lecture code demo
+var questionSet = ([
+  {
+    "question": "Was I born on the moon?",
+    "answer": "Y",
+    "correctFollowup": "Nice, I was born on the moon",
+    "incorrectFollowup": "No, I was actually born on the moon"
+  },
+  {
+    "question": "Am I a Taurus?",
+    "answer": "N",
+    "correctFollowup": "Correct, I'm actually a Saggitarius",
+    "incorrectFollowup": "No, I'm not a Taurus at all.. Do I act like one?"
+  },
+  {
+    "question": "Was I born on the moon",
+    "answer": "Y",
+    "correctFollowup": "Nice, I was born on the moon",
+    "incorrectFollowup": "No, I was actually born on the moon"
+  }
+])
 
 var correctResponses = 0;
-function askQuestion (question, correctAnswer, correctFollowup, incorrectFollowup, ) { // Expecting Y or N passed as correctAnswer
-  console.log ('Asking '+question+', Correct answer is '+correctAnswer);
-  var answer = prompt(question);
+function askQuestion (questionAsked, correctAnswer, correctFollowup, incorrectFollowup) { // Expecting Y or N passed as correctAnswer
+  console.log ('Asking '+questionAsked+', Correct answer is '+correctAnswer);
+  var answer = prompt(questionAsked);
   console.log ('Response: '+answer);
   modAnswer = answer[0].toUpperCase();
   console.log (modAnswer);
 
   if (modAnswer === correctAnswer) {
+    alert (correctFollowup);
     return true;
   } else  {
+    alert (incorrectFollowup);
     return false;
   }
 }
 
-var userName = prompt('What is your name?');
-console.log('The user said their name is ' + userName);
+// var userName = prompt('What is your name?');
+// console.log('The user said their name is ' + userName);
+//
+// alert('Hi, ' + userName + '!  Prepare for some questions about me. Please click "ok", then answer Y or N to the next question.');
 
-alert('Hi, ' + userName + '!  Prepare for some questions about me. Please click "ok", then answer Y or N to the next question.');
+for (var i=0;i<questionSet.length;i++) {
+  if (askQuestion (questionSet[i].question,questionSet[i].answer, questionSet[i].correctFollowup, questionSet[i].incorrectFollowup)) {
+    correctResponses++;
+  }
+}
 
-console.log(askQuestion ('Was I born on the moon','Y', ""));
-
+console.log (correctResponses);
 
 // var userResponse = prompt('Was, I born on the moon?');
 // console.log('userResponse: ' + userResponse);
