76797cbe92ca5bdfa286e84d87b790b18d5dc2e2
goodwid <goodwid@gmail.com>
2016-03-15 16:12:43 -0700

    Added demo code

--- page/js/lab.js
+++ page/js/lab.js
@@ -1,34 +1,18 @@
+// 201
+// 3-tue-lecture code demo
 
-var resp1, resp2, resp3, resp4, resp5, resp6;
+var userName = prompt('What is your name?');
+console.log('The user said their name is ' + userName);
 
-var query1 = "Hi, how are you?";
-var query2 = "What did you have for breakfast?";
-var query3 = "Did you have tea or coffee this morning?";
-var query4 = "Where are you going for lunch today?";
-var query5 = "Do you have a cat or a dog?";
+alert('Hey there, ' + userName + ' I\'m going to ask you some questions about me. Please click "ok", then answer Y or N to the next question.');
 
-resp1 = prompt (query1);
-console.log ('Asking:' + query1 + ', response: ' + resp1);
-resp2 = prompt (query2);
-console.log ('Asking:' + query2 + ', response: ' + resp2);
-resp3 = prompt (query3);
-console.log ('Asking:' + query3 + ', response: ' + resp3);
-resp4 = prompt (query4);
-console.log ('Asking:' + query4 + ', response: ' + resp4);
-resp5 = prompt (query5);
-console.log ('Asking:' + query5 + ', response: ' + resp5);
-var query6 = "What is your " + resp5 +  "'s name?";
-resp6 = prompt (query6);
-console.log ('Asking:' + query6 + ', response: ' + resp6);
+var userResponse = prompt('Was, I born on the moon?');
+console.log('userResponse: ' + userResponse);
 
-var alertText = '';
-
-alertText += 'Excellent, I am ' + resp1 + ' as well.  ';
-alertText += 'I did not have ' + resp2 + ', I had a bagel.  I like bagels better than '+ resp2 +'s ';
-alertText += 'and I prefer milk to ' + resp3 + '.  ';
-alertText += 'Instead of '+ resp4 +", I'm going to Chipotle.  Yum!  ";
-alertText += 'I like ' + resp5 +'s but I have a bird.  ';
-alertText += 'I am sure ' + resp6 + " is a fine name in some cultures but I don't like it.  ";
-
-alert (alertText);
-document.getElementById('entry').innerHTML= alertText;
+if (userResponse.toLowerCase() === 'n' || userResponse.toUpperCase() === 'NO') {
+  alert('Nope, I was actualy born on the moon. I\'m a moonian computer language.');
+} else if (userResponse === 'Y') { // TODO: Make this more robust!
+  alert('Nice, I was born on the moon');
+} else {
+  alert('Ooops!, you should have given a Y or N, so you lose this round.');
+}
