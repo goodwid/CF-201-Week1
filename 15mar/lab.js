9bbb59ccbb76c5b5c26f4428442d0b97ae36ed7d
David Goodwin <goodwid@gmail.com>
2016-03-15 09:10:26 -0700

    first commit

---
+++ 15mar/lab.js
@@ -0,0 +1,19 @@
+
+var resp1, resp2, resp3, resp4, resp5, resp6;
+
+var query1 = "Hi, how are you?";
+var query2 = "";
+var query3 = "";
+var query4 = "";
+var query5 = "";
+var query6 = "";
+
+resp1 = prompt (query1);
+console.log ('Asking:' + query1 + ', response: ' + resp1);
+//resp2 = prompt (query2);
+//resp3 = prompt (query3);
+//resp4 = prompt (query4);
+//resp5 = prompt (query5);
+//resp6 = prompt (query6);
+
+var alertText;
