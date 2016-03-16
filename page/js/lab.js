e84c446091b1ff46cd21f3754247257c46231c60
goodwid <goodwid@gmail.com>
2016-03-16 10:33:54 -0700

    added guessing game which is broken.

--- page/js/lab.js
+++ page/js/lab.js
@@ -27,8 +27,6 @@ var questionSet = [
 var correctResponses = 0;
 
 function askQuestion (questionAsked, correctAnswer, correctFollowup, incorrectFollowup) { // Expecting Y or N passed as correctAnswer
-    var noTest = new RegExp ('[Nn,Oo]');
-    var yesTest = new RexExp ('[Yy,Ee,Ss]');
     var validResponse = false;
 
     console.log ('Asking '+questionAsked+', Correct answer is '+correctAnswer);
@@ -36,11 +34,12 @@ function askQuestion (questionAsked, correctAnswer, correctFollowup, incorrectFo
       var answer = prompt(questionAsked);
       console.log ('Response: '+answer);
 
-      if (noTest.test(answer) && yesTest.test(answer)) {
-        validResponse = true
+      if (/^[Nn,Oo]{0,2}$/.test(answer) || /^[Yy,Ee,Ss]{0,3}$/.test(answer)) {
+        console.log ('Response valid.');
+        validResponse = true;
       } else {
+        console.log ('Response not valid, re-asking');
         alert ('Sorry, that is not a valid response, please answer Yes or No.');
-        console.log ('Respons not valid, re-asking');
       }
     }
     var modAnswer = answer[0].toUpperCase();
@@ -62,6 +61,8 @@ console.log('The user said their name is ' + userName);
 
 alert('Hi, ' + userName + '!  Prepare for some questions about me. Please click "ok", then answer Y or N to the next question.');
 
+// Asking all the Y-N questions..
+
 for (var i=0;i<questionSet.length;i++) {
   if (askQuestion (questionSet[i].question,questionSet[i].answer, questionSet[i].correctFollowup, questionSet[i].incorrectFollowup)) {
     correctResponses++;
@@ -69,8 +70,43 @@ for (var i=0;i<questionSet.length;i++) {
   console.log ('Asking question '+ i);
 }
 
+// Asking the high-low question
+var rightNumber = 86;
+var numberGuessed = false;
+var validResponse = false;
+console.log ('Asking guessing game question');
+alert ('OK, time for a guessing game!');
+while (!numberGuessed) {
+  answer = prompt ('Pick a number between 1 and 100');
+  console.log ('Response: '+ answer);
+  while (validResponse) {
+    if (answer < 1 || answer > 100) {
+      console.log ('Response valid.');
+      validResponse = true;
+    } else {
+        console.log ('Response not valid, re-asking');
+        alert ('Sorry, that is not a valid response, please pick a number between 1 and 100');
+    }
+  } // while validResponse
+  if (answer == rightNumber) {
+    console.log ('Correct answer guessed');
+    alert ('You nailed it! ' +rightNumber +' is right!');
+    correctResponses++;
+    numberGuessed = true;
+  } else if ((answer == rightNumber-1) || (answer == rightNumber+1)) {
+    alert ('Close enough, it was '+rightNumber +'.');
+    correctResponses++;
+    numberGuessed = true;
+  } else {
+    alert ('Sorry, that is not correct.  It was ' + rightNumber + '.');
+  }
+} // while !numberguessed
+
+
 console.log ('Number of questions answer correctly: ' + correctResponses);
-if (correctResponses < questionSet.length/2) {
+if (correctResponses === 0) {
+    alert ('Sorry, you got no answers correct.');
+} else if (correctResponses < questionSet.length/2) {
   alert ('Sorry, you only got ' + correctResponses + ' answers correct.');
 } else {
   alert ('Congratulations, you got ' + correctResponses + ' answers correct!');
